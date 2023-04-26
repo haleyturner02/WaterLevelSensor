a6fd0c81ec2a1fee8e4e967b80171ea65ff5e514
haleyturner02 <124711735+haleyturner02@users.noreply.github.com>
2023-04-25 18:30:33 -0600

    Update master.c

--- master.c
+++ master.c
@@ -17,11 +17,12 @@ volatile unsigned char transmit_key;
 
 
 char packet[] = {0x12, 0x0A, 0x0A, 0x0A, 0x0A};     // 5 byte packet for transmission
+//char packet[] = {0x12, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A};     // 6 byte packet
 
 volatile unsigned char col_holding, row_holding;
 volatile unsigned char pressed_key;
-//int lock_state = 0;
 int lock_state = 1;
+int enableLED = 1;
 
 void initI2C_master(){
      UCB1CTLW0 |= UCSWRST;          // SW RESET ON
@@ -58,7 +59,7 @@ void initTimerB0compare(){      // Setup TB0
     TB0CTL |= MC__UP;           // UP mode
 }
 
-void initTimerB1compare() {
+void initTimerB1compare() {     // Setup TB1
     TB1CTL |= TBCLR;            // Clear TB1
     TB1CTL |= TBSSEL__SMCLK;    // Select SMCLK
     TB1CTL |= MC__UP;           // UP mode
@@ -79,7 +80,7 @@ void configureAdc() {
 
 }
 
-void initLEDs() {
+void initLEDs() {                           // Setup LEDs
     P2DIR |= (BIT0 | BIT1 | BIT2 | BIT3);
     P2OUT &= ~(BIT0 | BIT1 | BIT2 | BIT3);
 
@@ -203,36 +204,33 @@ void getWaterLevel() {
 
     int digit;
 
-    /*if(avg >= 0 && avg <= 100) {                    // No water detected
-        key = 0x0B;
-    } else if(avg > 100 && avg <= 300) {            // Low water level
-        key = 0x0C;
-    } else if(avg > 300 && avg <= 360) {            // Mid water level
-        key = 0x0D;
-    } else if(avg > 360 && avg <= 400) {            // High water level
-        key = 0x0E;
-    } else if(avg > 400) {                          // Full water level
-        key = 0x0F;
-    }
-    */
-
     initLEDs();
 
-    if(avg >= 0 && avg <= 10) {
+    if(avg >= 0 && avg <= 10) {                 // Empty
         key = 0x0B;
-        P2OUT |= BIT0;
-    } else if(avg > 10 && avg <= 200) {
+        if(enableLED == 1) {
+            P2OUT |= BIT0;
+        }
+    } else if(avg > 10 && avg <= 200) {         // Low
         key = 0x0C;
-        P2OUT |= BIT1;
-    } else if(avg > 200 && avg <= 325) {
+        if(enableLED == 1) {
+            P2OUT |= BIT1;
+        }
+    } else if(avg > 200 && avg <= 325) {        // Good
         key = 0x0D;
-        P2OUT |= BIT2;
-    } else if(avg > 325 && avg <= 400) {
+        if(enableLED == 1) {
+            P2OUT |= BIT2;
+        }
+    } else if(avg > 325 && avg <= 400) {        // High
         key = 0x0E;
-        P2OUT |= BIT3;
-    } else if(avg > 400){
+        if(enableLED == 1) {
+            P2OUT |= BIT3;
+        }
+    } else if(avg > 400){                       // Full
         key = 0x0F;
-        P4OUT |= BIT0;
+        if(enableLED == 1) {
+            P4OUT |= BIT0;
+        }
     }
 
     packet[1] = key;                                // Set second byte in packet to indicate water level
@@ -250,6 +248,7 @@ void getWaterLevel() {
         }
         packet[i] = getCharKey(digit);              // Store each digit in the packet
     }
+    //packet[5] = getCharKey(n);
 }
 
 
@@ -278,7 +277,7 @@ int main(void) {
     initTimerB1compare();               // Initialize Timer B1 for sampling temperature sensor every 333ms
     configureAdc();                     // Initialize ADC for receiving input signal from temperature sensor
 
-    initLEDs();
+    initLEDs();                         // Intialize indicator LEDs
 
     columnInput();                      // Configure keypad columns to start as inputs
 
@@ -302,7 +301,7 @@ int main(void) {
     return 0;
 }
 
-void displayUnlockPattern() {
+void displayUnlockPattern() {           // Pattern to indicate system has been successfully unlocked
     int i;
     initLEDs();
     for(i = 0; i <= 32000; i++) {}
@@ -349,71 +348,70 @@ int is_unlocked(char k){            // Password: *#06*
     }
 }
 
+void relock() {
+    int i;
+
+    lock_state = 1;
+    packet[0] = 0x12;
+
+    for(i = 1; i < sizeof(packet); i++) {
+        packet[i] = 0x0A;
+    }
+
+    initLEDs();
+    n = 0;
+}
+
 
 void keyPressedAction(char pressed_key) {
     if(is_unlocked(pressed_key) == 1) {
         switch(pressed_key) {                   // Get pressed key
             case 0x87:
-                key = 0x01;     // 1
                 n = 1;
                 break;
             case 0x83:
-                key = 0x02;     // 2
                 n = 2;
                 break;
             case 0x81:
-                key = 0x03;     // 3
                 n = 3;
                 break;
-            case 0x80:
-                key = 0x80;     // A
-                break;
             case 0x47:
-                key = 0x04;     // 4
                 n = 4;
                 break;
             case 0x43:
-                key = 0x05;     // 5
                 n = 5;
                 break;
             case 0x41:
-                key = 0x06;     // 6
                 n = 6;
                 break;
-            case 0x40:
-                key = 0x40;     // B
-                break;
             case 0x27:
-                key = 0x07;     // 7
                 n = 7;
                 break;
             case 0x23:
-                key = 0x08;     // 8
                 n = 8;
                 break;
             case 0x21:
-                key = 0x09;     // 9
                 n = 9;
                 break;
-            case 0x20:
-                key = 0x20;     // C
-                break;
-            case 0x17:
-                key = 0x17;     // *
-                break;
             case 0x13:
-                key = 0x00;     // 0
                 n = 0;
                 break;
-            case 0x11:
-                key = 0x11;     // #
+
+            case 0x80:                  // A- Relock system
+                relock();
                 break;
-            case 0x10:
-                key = 0x10;     // D
+            case 0x40:                  // B- Enable/disable LEDs
+                if(enableLED == 1) {
+                    enableLED = 0;
+                } else {
+                    enableLED = 1;
+                }
                 break;
             default:
                 break;
         }
+
+        //packet[5] = getCharKey(n);
 
     }
 
